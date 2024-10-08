6d19d6a74d4a2741994a1c8a1b4c6249439d20a4
Conor Harkin <Harkin-C36@ulster.ac.uk>
2024-10-08 11:27:27 +0100

    updated code with access modifiers

---
+++ Week3-Library/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Library
+{
+    class Book
+    {
+        //Properties
+        string Title;
+        string Author;
+        string ISBN;
+        int Pages;
+
+        //Constructor for Book object
+        public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+            Pages = bookPages;
+        }
+
+        public void DisplayInfo()
+        {
+            //output the book information to the console
+            Console.WriteLine("Book Information:");
+            Console.WriteLine("--------------");
+            Console.WriteLine($"Book Title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book Pages: {Pages}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,36 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+using Week3_Library;
 
-class Book
-{
-    //Properties
-    string Title;
-    string Author;
-    string ISBN;
-    int Pages;
-
-    //Constructor for Book object
-    public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-        Pages = bookPages;
-    }
-
-    void DisplayInfo()
-    {
-        //output the book information to the console
-        Console.WriteLine("Book Information:");
-        Console.WriteLine("--------------");
-        Console.WriteLine($"Book Title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book Pages: {Pages}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
-
-
+class Program { 
     static void Main(string[] args)
     {
         //create a new instance of the book class
