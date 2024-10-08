24fa866360ff9cc917e960db54f61cf9f5dccc5e
Conor Harkin <Harkin-C36@ulster.ac.uk>
2024-10-08 11:38:52 +0100

    Updated code with variables and properties.

--- Week3-Library/Book.cs
+++ Week3-Library/Book.cs
@@ -9,10 +9,33 @@ namespace Week3_Library
     class Book
     {
         //Properties
-        string Title;
-        string Author;
-        string ISBN;
-        int Pages;
+        private string title;
+        private string author;
+        private string isbn;
+        private int pages;
+
+        // Title property to allow access
+        // to the title private variable
+        public string Title
+        {
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+        public int Pages
+        {
+            get { return pages; }
+            set { pages = value; }
+        }
 
         //Constructor for Book object
         public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
